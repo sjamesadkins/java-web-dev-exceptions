32d6c4aff9ba4c94770e6af48fa1a93801ecd057
SAM ADKINS <sjamesadkins@gmail.com>
2023-02-08 21:42:31 -0600

    Finished exercises for chapter 11

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -16,16 +16,47 @@ public class Main {
         studentFiles.put("Stefanie", "CoolProgram.java");
 
         // Test out your CheckFileExtension() function!
+
+        Divide(3, 0);
+
+        for (Map.Entry<String, String> student : studentFiles.entrySet()) {
+            System.out.println(student.getKey() + " receives a value of " + CheckFileExtension(student.getValue()));
+        }
+
     }
 
-    public static void Divide(int x, int y)
-    {
-        // Write code here!
+    public static void Divide(int x, int y) {
+        if(y == 0) {
+            try {
+                throw new ArithmeticException("Cannot divide by zero! Please enter a non-zero number for y.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println(x/y);
+        }
     }
 
-    public static int CheckFileExtension(String fileName)
-    {
-        // Write code here!
-    }
 
+    public static int CheckFileExtension(String fileName) {
+        int points = 0;
+        if (fileName == null) {
+            try {
+                throw new Exception("File Name Extension must exist");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            points = -1;
+        } else if (fileName.equals("")) {
+            try {
+                throw new Exception("File Name Extension must not be empty. Score of '-1' assigned to student with missing File Name Extension");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            points = -1;
+        } else if (fileName.endsWith(".java")) {
+                points = 1;
+        }
+        return points;
+    }
 }
